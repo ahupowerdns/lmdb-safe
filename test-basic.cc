505d31fd89457969401de77dd9c399aaf3d5bdbe
bert hubert <bert.hubert@netherlabs.nl>
2019-01-05 14:40:04 +0100

    add range tests (lower_bound)

--- test-basic.cc
+++ test-basic.cc
@@ -28,3 +28,65 @@ TEST_CASE("Most basic tests", "[mostbasic]") {
   auto rotxn = env.getROTransaction();
   REQUIRE(rotxn.get(main, "lmdb", out) == MDB_NOTFOUND);
 }
+
+TEST_CASE("Range tests", "[range]") {
+  unlink("./tests");
+
+  MDBEnv env("./tests", MDB_NOSUBDIR, 0600);
+  REQUIRE(1);
+
+  MDBDbi main = env.openDB("", MDB_CREATE);
+  
+  auto txn = env.getRWTransaction();
+  MDBOutVal out;
+
+  REQUIRE(txn.get(main, "lmdb", out) == MDB_NOTFOUND);
+  
+  txn.put(main, "bert", "hubert");
+  txn.put(main, "bertt", "1975");  
+  txn.put(main, "berthubert", "lmdb");
+  txn.put(main, "bert1", "one");
+  txn.put(main, "beru", "not");  
+  
+  {
+    auto cursor = txn.getCursor(main);
+    MDBInVal bert("bert");
+    MDBOutVal key, val;
+    REQUIRE(cursor.lower_bound(bert, key, val) == 0);
+    REQUIRE(key.get<string>() == "bert");
+    REQUIRE(val.get<string>() == "hubert");
+
+    REQUIRE(cursor.next(key, val) == 0);
+    REQUIRE(key.get<string>() == "bert1");
+    REQUIRE(val.get<string>() == "one");
+
+    REQUIRE(cursor.next(key, val) == 0);
+    REQUIRE(key.get<string>() == "berthubert");
+    REQUIRE(val.get<string>() == "lmdb");
+
+    REQUIRE(cursor.lower_bound("kees", key, val) == MDB_NOTFOUND);  
+
+    txn.commit();
+  }
+  
+  auto rotxn = env.getROTransaction();
+  {
+    auto cursor = rotxn.getCursor(main);
+    MDBInVal bert("bert");
+    MDBOutVal key, val;
+    REQUIRE(cursor.lower_bound(bert, key, val) == 0);
+    REQUIRE(key.get<string>() == "bert");
+    REQUIRE(val.get<string>() == "hubert");
+
+    REQUIRE(cursor.next(key, val) == 0);
+    REQUIRE(key.get<string>() == "bert1");
+    REQUIRE(val.get<string>() == "one");
+
+    REQUIRE(cursor.next(key, val) == 0);
+    REQUIRE(key.get<string>() == "berthubert");
+    REQUIRE(val.get<string>() == "lmdb");
+
+    REQUIRE(cursor.lower_bound("kees", key, val) == MDB_NOTFOUND);  
+  }
+    
+}
